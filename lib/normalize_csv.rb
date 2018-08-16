037b9c4cb50baed905ea333917ed799b8e74b26f
John Gedeon <johnw@gedeons.com>
2018-08-16 12:24:08 -0700

    Add class to process full csv file

--- lib/normalize_csv.rb
+++ lib/normalize_csv.rb
@@ -1,7 +1,16 @@
-# frozen_string_literal: true
-
 require 'normalize_csv/version'
+require 'normalize_csv/parser'
+require 'normalize_csv/output'
 
 module NormalizeCsv
-  # Your code goes here...
+  def self.process
+    STDIN.each_line do |line|
+      if line.strip == "Timestamp,Address,ZIP,FullName,FooDuration,BarDuration,TotalDuration,Notes"
+        STDOUT.puts NormalizeCsv::Output.csv_header
+      else
+        parsed_row = NormalizeCsv::Parser.parse(line.strip)
+        STDOUT.puts NormalizeCsv::Output.csv_row(parsed_row) unless parsed_row.nil?
+      end
+    end
+  end
 end
--- spec/normalize_csv_spec.rb
+++ spec/normalize_csv_spec.rb
@@ -1,11 +1,75 @@
-# frozen_string_literal: true
+require 'normalize_csv/row'
 
 RSpec.describe NormalizeCsv do
   it 'has a version number' do
     expect(NormalizeCsv::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#process' do
+    let(:csv_header) { "Timestamp,Address,ZIP,FullName,FooDuration,BarDuration,TotalDuration,Notes\n" }
+    let(:csv_line) { '4/1/11 11:00:00 AM,"123 4th ☃  St, Anywhere, AA",94121,Monkey 🐵 Alberto,1:2:3.123,1:2:3.123,zzsasdfa⚑,I am the very model of a modern major general ⚑' }
+    let(:csv_line2) { '5/1/11 11:00:00 AM,"124 4th ☃  St, Anywhere, AA",94121,Monkey 🐵 Alberto,1:2:3.123,1:2:3.123,zzsasdfa⚑,I am the very model of a modern major general ⚑' }
+    let(:expected_row) do
+      NormalizeCsv::Row.new(
+        timestamp: Time.new(2011,4,1,11,0,0, "-07:00").in_time_zone('US/Pacific'),
+        address: '123 4th ☃  St, Anywhere, AA',
+        zip: 94121,
+        full_name: 'Monkey 🐵 Alberto',
+        foo_duration: 3723.123,
+        bar_duration: 3723.123,
+        notes: 'I am the very model of a modern major general ⚑'
+      )
+    end
+    let(:expected_row2) do
+      NormalizeCsv::Row.new(
+        timestamp: Time.new(2011,5,1,11,0,0, "-07:00").in_time_zone('US/Pacific'),
+        address: '124 4th ☃  St, Anywhere, AA',
+        zip: 94121,
+        full_name: 'Monkey 🐵 Alberto',
+        foo_duration: 3723.123,
+        bar_duration: 3723.123,
+        notes: 'I am the very model of a modern major general ⚑'
+      )
+    end
+
+    before do
+      allow(STDIN).to receive(:each_line)
+        .and_yield(csv_header).and_yield(csv_line).and_yield(csv_line2)
+      allow(NormalizeCsv::Parser).to receive(:parse).with(csv_line).and_return(expected_row)
+      allow(NormalizeCsv::Parser).to receive(:parse).with(csv_line2).and_return(expected_row2)
+      allow(NormalizeCsv::Output).to receive(:csv_header).and_return('first row')
+      allow(NormalizeCsv::Output).to receive(:csv_row).with(expected_row).and_return('second row')
+      allow(NormalizeCsv::Output).to receive(:csv_row).with(expected_row2).and_return('third row')
+      allow(STDOUT).to receive(:puts)
+    end
+
+    it 'reads line from STDIN and calls parse for each except header' do
+      expect(NormalizeCsv::Parser).to receive(:parse).with(csv_line)
+      expect(NormalizeCsv::Parser).to receive(:parse).with(csv_line2)
+      NormalizeCsv.process
+    end
+
+    it 'passes row object to output' do
+      expect(NormalizeCsv::Output).to receive(:csv_row).with(expected_row)
+      expect(NormalizeCsv::Output).to receive(:csv_row).with(expected_row2)
+      NormalizeCsv.process
+    end
+
+    it 'calls for the header' do
+      expect(NormalizeCsv::Output).to receive(:csv_header)
+      NormalizeCsv.process
+    end
+
+    it 'prints all to STDOUT in order' do
+      expect(STDOUT).to receive(:puts).with('first row').ordered
+      expect(STDOUT).to receive(:puts).with('second row').ordered
+      expect(STDOUT).to receive(:puts).with('third row').ordered
+      NormalizeCsv.process
+    end
+
+    it 'handles nil parsed row' do
+      allow(NormalizeCsv::Parser).to receive(:parse).and_return(nil)
+      expect{NormalizeCsv.process}.not_to raise_error
+    end
   end
 end
