4fd070250f85b60c3a1593e365199b6d985f9f0a
John Gedeon <johnw@gedeons.com>
2018-08-16 01:30:08 -0700

    Add parser class

---
+++ lib/normalize_csv/parser.rb
@@ -0,0 +1,53 @@
+require 'csv'
+require 'time'
+require 'active_support/time'
+
+module NormalizeCsv
+  module Parser
+    def self.parse(csv_line)
+      CSV.parse(csv_line) do |row|
+        return Row.new(
+          timestamp: parse_timestamp(row[0]),
+          address: parse_address(row[1]),
+          zip: parse_zip(row[2]),
+          full_name: parse_full_name(row[3]),
+          foo_duration: parse_duration(row[4]),
+          bar_duration: parse_duration(row[5]),
+          notes: parse_notes(row[7])
+        )
+      end
+    end
+
+    private
+
+    def self.parse_timestamp(timestamp)
+      ENV['TZ'] = 'US/Pacific'
+      Time.strptime(timestamp, '%m/%d/%y %I:%M:%S %p').in_time_zone('US/Pacific')
+    end
+
+    def self.parse_address(address)
+      address
+    end
+
+    def self.parse_zip(zip)
+      zip.to_i
+    end
+
+    def self.parse_full_name(full_name)
+      full_name
+    end
+
+    def self.parse_duration(duration)
+      matches = /(?<hours>\d\d?):(?<minutes>\d\d?):(?<seconds>\d\d?).(?<milliseconds>\d+)/.match(duration)
+
+      duration = (matches[:hours].to_i * 60 * 60) +
+        (matches[:minutes].to_i * 60) +
+        matches[:seconds].to_i +
+        (matches[:milliseconds].to_i * 0.001)
+    end
+
+    def self.parse_notes(notes)
+      notes
+    end
+  end
+end
---
+++ spec/normalize_csv/parser_spec.rb
@@ -0,0 +1,54 @@
+require 'normalize_csv/parser'
+require 'normalize_csv/row'
+
+RSpec.describe NormalizeCsv::Parser do
+  describe '#parse' do
+    describe 'with plain ascii string' do
+      let(:csv_line) { '4/1/11 11:00:00 AM,"123 4th St, Anywhere, AA",94121,Monkey Alberto,1:2:3.123,1:2:3.123,zzsasdfa,I am the very model of a modern major general' }
+      let(:expected_row) do
+        NormalizeCsv::Row.new(
+          timestamp: Time.new(2011,4,1,11,0,0, "-07:00").in_time_zone('US/Pacific'),
+          address: '123 4th St, Anywhere, AA',
+          zip: 94121,
+          full_name: 'Monkey Alberto',
+          foo_duration: 3723.123,
+          bar_duration: 3723.123,
+          notes: 'I am the very model of a modern major general'
+        )
+      end
+      let(:result) { NormalizeCsv::Parser.parse(csv_line) }
+
+      it 'returns row object' do
+        expect(result).to be_a_kind_of(NormalizeCsv::Row)
+      end
+
+      it 'with correct timestamp' do
+        expect(result.timestamp).to eq(expected_row.timestamp)
+      end
+
+      it 'with correct address' do
+        expect(result.address).to eq(expected_row.address)
+      end
+
+      it 'with correct zip' do
+        expect(result.zip).to eq(expected_row.zip)
+      end
+
+      it 'with correct full_name' do
+        expect(result.full_name).to eq(expected_row.full_name)
+      end
+
+      it 'with correct foo_duration' do
+        expect(result.foo_duration).to eq(expected_row.foo_duration)
+      end
+
+      it 'with correct bar_duration' do
+        expect(result.bar_duration).to eq(expected_row.bar_duration)
+      end
+
+      it 'with correct notes' do
+        expect(result.notes).to eq(expected_row.notes)
+      end
+    end
+  end
+end
